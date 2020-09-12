18a873e8b6041678af11a9a1fd4fe7d5581eaac8
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2020-09-12 11:18:42 +0530

    Add files via upload

---
+++ Sum of one integer & one real value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int n1;
+	float n2,sum;
+	printf("Enter any two number : ");
+	scanf("%d%f",&n1,&n2);
+	sum=n1+n2;
+	printf("Sum = %0.2f",sum);//printing sum of two numbers
+}

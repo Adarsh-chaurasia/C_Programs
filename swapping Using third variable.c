07f467095e45ee24e109763106463e86db28442d
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2020-09-12 11:20:25 +0530

    Add files via upload

---
+++ swapping Using third variable.c
@@ -0,0 +1,10 @@
+//swapping of two integer numbers using third variable.!!
+#include<stdio.h>
+main()
+{
+	int n1,n2,temp;
+	printf("Enter all the two numbers x and y :  ");
+	scanf("%d%d",&n1,&n2);
+	temp=n1;n1=n2;n2=temp;
+	printf("x=%d\ny=%d",n1,n2);
+}
